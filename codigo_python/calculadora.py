74baa69e63fe3fb5db37ed449b9439c770ae760e
Sh1k4maru <shadow.cat6333@gmail.com>
2023-06-19 16:10:24 -0600

    Agrega

--- codigo_python/calculadora.py
+++ codigo_python/calculadora.py
@@ -1,7 +1,10 @@
 class Calculadora:
-   
     def sumar(self, num1, num2):
         return num1 + num2
 
     def restart(self, num1, num2):
         return num1 - num2
+
+
+if __name__=="__main__":
+    print('ola')
